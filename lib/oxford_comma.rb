7b1ea15d2d2695df8d6f23fa555a92538f17a30a
Redmond Chan <redmondchan92@gmail.com>
2018-12-04 17:52:36 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length ==2
+    array.join(" and ")
+  elsif array.length == 3
+    x = array.slice(2)
+    y = x.length
+    z = array.join(", ")
+    z.insert((y+1)*(-1), "and ")
+  else array.length > 3
+    a = array.length - 1
+    b = array.slice(a)
+    c = b.length
+    d = array.join(", ")
+    d.insert((c+1)*-1, "and ")
+  end
 end
